346f40b341f618c28afb17f8c6008022002f5c26
Kimthangg <daokimthang.work@gmail.com>
2024-05-25 13:56:28 +0700

    Kimthang

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def lower_tokenize(text):
     text = re.sub(r'[^\w\s]', '', text)
     text = re.sub(r'\d+', '', text)
     tokens = underthesea.word_tokenize(text)
-    print("Lower",tokens)
     return tokens
 
 def remove_stopword(tokens):
@@ -17,11 +16,10 @@ def remove_stopword(tokens):
     stopwords = [word.strip() for word in stopwords]
     final_tokens = [token for token in tokens if token not in stopwords]
     final_text = ' '.join(final_tokens)
-    print("Stopword",final_text)
     return final_text
 
 def run():
-    tfidf_vec,MultiNB,encoder=joblib.load('Model/model.joblib')
+    tfidf_vectorizer,MultiNB,encoder=joblib.load('Model/model.joblib')
     st.set_page_config(page_title="Hệ thống phân loại văn bản")
 
     st.write("# Hệ thống phân loại văn bản")
@@ -29,7 +27,7 @@ def run():
     sentence = st.text_area('Nhập văn bản:')
     if st.button('Submit', type='primary') and sentence:
         sentence=remove_stopword(lower_tokenize(sentence))
-        vector_sentence = tfidf_vec.transform([sentence])
+        vector_sentence = tfidf_vectorizer.transform([sentence])
         final_sentence=MultiNB.predict(vector_sentence)[0]
         final_sentence=encoder.inverse_transform([final_sentence])[0]
         st.title(f'Với văn bản trên, thể loại của văn bản sẽ thuộc: {final_sentence}')
